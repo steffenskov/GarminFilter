7ed4616f09e98a911fad54aee0f956819a806d5f
Steffen Skov <steffen@ckode.dk>
2025-10-22 18:45:09 +0200

    fix: Devices that are aliases (e.g. Quatix 8) are missing (#7)

--- backend/src/GarminFilter.Api/Models/DeviceViewModel.cs
+++ backend/src/GarminFilter.Api/Models/DeviceViewModel.cs
@@ -3,14 +3,50 @@ using GarminFilter.Domain.Device.ValueObjects;
 
 namespace GarminFilter.Api.Models;
 
-public class DeviceViewModel
+public record DeviceViewModel
 {
-	public DeviceViewModel(GarminDevice device)
+	private readonly string? _urlName;
+
+	private DeviceViewModel(GarminDevice device)
 	{
 		Id = device.Id;
-		Name = device.Name.Capitalize();
+		Name = StripCharacters(device.Name).Capitalize();
+		_urlName = device.UrlName;
 	}
 
-	public string Name { get; }
+	public string Name { get; private init; }
 	public DeviceId Id { get; }
+
+	private static string StripCharacters(string name)
+	{
+		return name
+			.Replace("™", "")
+			.Replace("®", "")
+			.Replace("ē", "e");
+	}
+
+	public static IEnumerable<DeviceViewModel> Create(GarminDevice device)
+	{
+		yield return new DeviceViewModel(device);
+		foreach (var additionalName in device.AdditionalNames)
+		{
+			yield return new DeviceViewModel(device with
+			{
+				Name = additionalName
+			});
+		}
+	}
+
+	public DeviceViewModel IncludeDistinctName()
+	{
+		if (string.IsNullOrWhiteSpace(_urlName))
+		{
+			return this;
+		}
+
+		return this with
+		{
+			Name = $"{Name} ({_urlName})"
+		};
+	}
 }
--- backend/src/GarminFilter.Domain/Device/Aggregates/GarminDevice.cs
+++ backend/src/GarminFilter.Domain/Device/Aggregates/GarminDevice.cs
@@ -5,16 +5,21 @@ namespace GarminFilter.Domain.Device.Aggregates;
 public record GarminDevice : IAggregate<DeviceId>
 {
 	public required string Name { get; init; }
+	public string[] AdditionalNames { get; init; } = [];
+	public string? UrlName { get; init; }
 	public required DeviceId Id { get; init; }
 
-	public GarminDevice StripCharacters()
+
+	public virtual bool Equals(GarminDevice? other)
 	{
-		return this with
-		{
-			Name = Name
-				.Replace("™", "")
-				.Replace("®", "")
-				.Replace("ē", "e")
-		};
+		if (other is null)
+			return false;
+		
+		return Id.Equals(other.Id) &&  Name.Equals(other.Name) && UrlName?.Equals(other.UrlName) != false && AdditionalNames.SequenceEqual(other.AdditionalNames);
+	}
+
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(Name, AdditionalNames, UrlName, Id);
 	}
 }
