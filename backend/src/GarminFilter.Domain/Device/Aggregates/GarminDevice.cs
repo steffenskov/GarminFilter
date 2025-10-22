3944023ecfc6b994df8f52ea57d95c9f0351d06e
steffenskov <steffen@ckode.dk>
2025-10-22 18:40:41 +0200

    fix tests

--- backend/src/GarminFilter.Domain/Device/Aggregates/GarminDevice.cs
+++ backend/src/GarminFilter.Domain/Device/Aggregates/GarminDevice.cs
@@ -8,4 +8,18 @@ public record GarminDevice : IAggregate<DeviceId>
 	public string[] AdditionalNames { get; init; } = [];
 	public string? UrlName { get; init; }
 	public required DeviceId Id { get; init; }
+
+
+	public virtual bool Equals(GarminDevice? other)
+	{
+		if (other is null)
+			return false;
+		
+		return Id.Equals(other.Id) &&  Name.Equals(other.Name) && UrlName?.Equals(other.UrlName) != false && AdditionalNames.SequenceEqual(other.AdditionalNames);
+	}
+
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(Name, AdditionalNames, UrlName, Id);
+	}
 }
